76bbe8650662fa09c0d35619d7b21b9fc58018dc
thisIsMaamathi <maamathi.krishnan+cdwcorp@cdw.com>
2025-07-29 11:41:01 +0530

    Create index.js

---
+++ exercise/dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get('/', (_req, res) => {
+    res.json({ message: 'Hello from TypeScript app!' });
+});
+if (process.env.NODE_ENV !== 'test') {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+exports.default = app;
